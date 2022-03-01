cad7c991381bc238f223389b8fd339d5c33190b7
unknown <iamhasan9501@gmail.com>
2022-03-02 01:51:17 +0600

    show btn added

--- phone-search.js
+++ phone-search.js
@@ -1,3 +1,4 @@
+///////////////// Search Phone //////////////////
 const searchPhone = () => {
     const searchField = document.getElementById('search-field');
     const searchText = searchField.value;
@@ -9,55 +10,58 @@ const searchPhone = () => {
     if (searchText == '') {
         const numberOfResultsFoundDiv = document.getElementById('numberof-results-found');
         numberOfResultsFoundDiv.innerHTML = `<p class="text-danger text-center"> <b> You didn't type anything ! </b>    </p> `;
+        const showAllBtn = document.getElementById('show-all');
+        showAllBtn.textContent = '';
 
     }
     else {
-        const url = ` https://openapi.programming-hero.com/api/phones?search=${searchText}`;
-        fetch(url)
+        fetch(` https://openapi.programming-hero.com/api/phones?search=${searchText}`)
             .then(response => response.json())
             .then(info => displaySearchResults(info.data))//here 2nd '.data'=>  from api//
     }
 }
-
+//////////////////////////////////// Display Search Results/////////////////////////////
 const displaySearchResults = data => {
-    console.log(data.length);
-    const twenty = data.slice(0, 20);
-    console.log(twenty);
+    // console.log(data.length);
     const searchResultDiv = document.getElementById('search-result-section');
     searchResultDiv.textContent = '';
     if (data.length == 0) {
         const numberOfResultsFoundDiv = document.getElementById('numberof-results-found');
         numberOfResultsFoundDiv.innerHTML = `<p class="text-danger text-center"> <b> No result found ! </b> </p> `;
+        const showAllBtn = document.getElementById('show-all');
+        showAllBtn.textContent = '';
     }
     else {
         const numberOfResultsFoundDiv = document.getElementById('numberof-results-found');
         numberOfResultsFoundDiv.innerHTML = `<p class="text-success text-center"> ${data.length} result(s) found </p> `;
-
-    }
-    twenty.forEach(phone => {
-        const Div = document.createElement('div');
-        Div.classList.add('col');
-        Div.innerHTML = ` <div class="col">
-        <div class="card h-50">
-            <img src="${phone.image}" class="card-img-top" alt="...">
-            <div class="card-body">
-                <h5 class="card-title">${phone.phone_name}</h5>
-                <p class="card-text">${phone.brand}</p>
-            <button  onclick="loadDetail('${phone.slug}')"  class="btn btn-success">Show Details</button>
-                <p class="card-text"></p>
+        const twentyResultsShow = data.slice(0, 20);
+        twentyResultsShow.forEach(phone => {
+            const Div = document.createElement('div');
+            Div.classList.add('col');
+            Div.innerHTML = ` <div class="col">
+            <div class="card h-50">
+                <img src="${phone.image}" class="card-img-top" alt="...">
+                <div class="card-body">
+                    <h5 class="card-title">${phone.phone_name}</h5>
+                    <p class="card-text">${phone.brand}</p>
+                <a  onclick="loadDetail('${phone.slug}')" href="#single-phone-details" class="btn btn-success">Show Details</a>
+                    <p class="card-text"></p>
+                </div>
             </div>
-        </div>
-    </div> ` ;
-        searchResultDiv.appendChild(Div);
-        // console.log(phone);
-    })
+        </div> 
+          ` ;
+            searchResultDiv.appendChild(Div);
+        })
+        const showAllDiv = document.getElementById('show-all');
+        showAllDiv.innerHTML = ` <button class="mb-5  mt-5 btn btn-info mx-auto"> Show All Results </button> `;
+    }
 }
-
+//////////////////////////////Load Details Fetch/////////////////////
 const loadDetail = id_slug => {
     const url = `https://openapi.programming-hero.com/api/phone/${id_slug}`;
     fetch(url).then(res => res.json()).then(info => loadPhoneDetails(info.data))  //here 2nd '.data'  from api
 }
-
+////////////////// Load Single Phone Details////////////////////////////
 const loadPhoneDetails = phoneDetails => {
     console.log(phoneDetails);
     const singleResultDiv = document.getElementById('single-phone-details');
@@ -76,18 +80,15 @@ const loadPhoneDetails = phoneDetails => {
         <p class="card-text">Sensors: <b>${phoneDetails.mainFeatures.sensors}</b></p>
        <p class="card-text text-success">Others Info   </p>
        <p class="card-text ">   
-                        Bluetooth: <b> ${phoneDetails.others.Bluetooth} </b>   <br>
-                        GPS: <b> ${phoneDetails.others.GPS} </b>   <br> 
-                        Radio: <b> ${phoneDetails.others.Radio} </b>   <br>
-                        USB: <b> ${phoneDetails.others.USB} </b>   <br>
-                        NFC: <b> ${phoneDetails.others.NFC} </b>  <br>
-                        WLAN: <b> ${phoneDetails.others.WLAN} </b>  <br>
+                        Bluetooth: <b> ${phoneDetails?.others?.Bluetooth} </b>   <br>
+                        GPS: <b> ${phoneDetails?.others?.GPS} </b>   <br> 
+                        Radio: <b> ${phoneDetails?.others?.Radio} </b>   <br>
+                        USB: <b> ${phoneDetails?.others?.USB} </b>   <br>
+                        NFC: <b> ${phoneDetails?.others?.NFC} </b>  <br>
+                        WLAN: <b> ${phoneDetails?.others?.WLAN} </b>  <br>
                     </p>
       
         <a href="#" class="btn btn-success">Show Video</a>
     </div>  `;
     singleResultDiv.appendChild(Div);
-
-
-
 }
